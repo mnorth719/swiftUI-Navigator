3dc36ed577a503f6316a12b4e830483cd31ef5a9
Matt North <matt.north93@gmail.com>
2021-01-05 17:31:50 -0800

    Initial Commit

---
+++ Navigator/NavigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigatorApp.swift
+//  Navigator
+//
+//  Created by Matt North on 1/5/21.
+//
+
+import SwiftUI
+
+@main
+struct NavigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
